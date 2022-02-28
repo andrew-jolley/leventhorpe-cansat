6ff32d05c4ab8a42420a01d872485a822c945809
Andrew Jolley <andrew.jolley145@gmail.com>
2022-02-28 14:12:53 +0000

    circuitpy

--- Code/bmp280-python-0.0.4/library/tests/tools.py
+++
@@ -1,7 +0,0 @@
-from i2cdevice import MockSMBus
-
-
-class SMBusFakeDevice(MockSMBus):
-    def __init__(self, i2c_bus):
-        MockSMBus.__init__(self, i2c_bus)
-        self.regs[0xD0] = 0x58         # Fake chip ID
---
+++ Code/code.py
@@ -0,0 +1,19 @@
+# importing other information
+import digitalio
+import board
+import time
+import radio
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+led.value = True
+
+packet_count = 0
+
+while True:
+    led.value = not led.value
+    radio_message = radio.try_read()
+    if radio_message is not None:
+        print("Radio Message: {:d} {:s}".format(packet_count, str(radio_message, 'ascii')))
+        packet_count = packet_count + 1
+    time.sleep(0.5)
