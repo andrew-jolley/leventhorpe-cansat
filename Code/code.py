59d59d984c18ca3ab0b44a8a84a464ce3ba05d96
Andrew Jolley <andrew.jolley145@gmail.com>
2022-03-05 08:26:46 +0000

    With CO2

--- Code/code.py
+++ Code/code.py
@@ -2,7 +2,9 @@
 import digitalio
 import board
 import time
+import bmp280
 import radio
+import carbon
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
@@ -12,8 +14,29 @@ packet_count = 0
 
 while True:
     led.value = not led.value
-    radio_message = radio.try_read()
-    if radio_message is not None:
-        print("Radio Message: {:d} {:s}".format(packet_count, str(radio_message, 'ascii')))
-        packet_count = packet_count + 1
+    print("Radio message sent")
+    time.sleep(1)
+
+    new_carbon = carbon.value()
+    new_temp = carbon.temp()
+    new_humidity = carbon.humidity()
+    new_altitude = carbon.altitude()
+
+
+    cansat_temperature = bmp280.read_temperature()
+    room_temp = bmp280.read_temperature()
+    #print(cansat_temperature)
+
+    cansat_pressure = bmp280.read_pressure()
+    room_pressure = bmp280.read_pressure()
+    #print(cansat_pressure)
+
+    print("CO2: {:.2f} PPM, Pressure: {:.2f} mbar, Temperature: {:.2f} degrees C, Temperature: {:.2f} degrees C, Humidity: {:.1f} %%rH, Altitude: {:.3f}M.".format(new_carbon, cansat_pressure, new_temp, cansat_temperature, new_humidity, new_altitude))
+
+    #print("Temperature: {:.3f}^C Pressure: QNH {:.2f}".format(cansat_temperature, cansat_pressure))
+
+    #radio.send("[LevCanSat] Temperature: {:.2f} Pressure {:.0f} mbar".format(room_temp, room_pressure))
+    radio.send("[LevCanSat] CO2: {:.2f} PPM, Pressure: {:.2f} mbar, Temperature: {:.2f} degrees C, Temperature: {:.2f} degrees C, Humidity: {:.1f} %%rH, Altitude: {:.3f}M.".format(new_carbon, cansat_pressure, new_temp, cansat_temperature, new_humidity, new_altitude))
+
+
     time.sleep(0.5)
